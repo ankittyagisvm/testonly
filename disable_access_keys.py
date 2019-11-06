31f23825139dbc733cfb1863c052cd51048a0f9d
Ankit Tyagi <39559769+ankittyagisvm@users.noreply.github.com>
2019-11-06 15:27:51 +0530

    Update disable_access_keys.py

--- disable_access_keys.py
+++ disable_access_keys.py
@@ -8,7 +8,7 @@ import boto3
 from botocore.exceptions import ClientError
 
 ######### upper code is to replace only 'import boto3'
-
+from datetime import date
 import datetime
 from dateutil.tz import tzutc
 from datetime import datetime, timedelta
@@ -43,39 +43,29 @@ def lambda_handler(event, context):
             for key in user.access_keys.all():
                 #print(key.user_name, key.id, key.status)
                 if key.status == "Active":
-                    for i in range(len(client.list_user_tags(UserName=key.user_name)['Tags'])):
-                        if client.list_user_tags(UserName=key.user_name)['Tags'][i]['Key']=='Owner':
-                            MAIL_id=client.list_user_tags(UserName=key.user_name)['Tags'][i]['Value']
-                    response = client.get_access_key_last_used(
-                        AccessKeyId=key.id
-                    )
-                    if 'LastUsedDate' in response['AccessKeyLastUsed']:
-                        #print(response['AccessKeyLastUsed']['LastUsedDate'])
-                        User_last_acess=response['AccessKeyLastUsed']['LastUsedDate'].date() # this is when last time key get accessed
-                        todays=datetime.now()  #   - timedelta(days=100)  
-                        date_only=todays.date()  # todays date
-                        use_polcy_diff = (date_only - User_last_acess).days
-                        if use_polcy_diff > 90:
-                            response = client.update_access_key(
-                                UserName = key.user_name,
-                                AccessKeyId = key.id,
-                                Status='Inactive'
-                            )
-                            print(key.user_name,"not using his access keys from last", use_polcy_diff, " days")
-                            Name = key.user_name
-                            username = os.environ['USERNAME']
-                            password = os.environ['PASSWORD']
-                            host = os.environ['SMTPHOST']
-                            port = os.environ['SMTPPORT']
-                            mail_from = os.environ['MAIL_FROM']
-                            #mail_to = os.environ['MAIL_TO']
-                            recpt = MAIL_id +',comscoresupport@3pillarglobal.com'
-                            mail_to = recpt
-                            print(mail_to)
-                            #reply_to = os.environ['MAIL_TO']
-                            reply_to = 'comscoresupport@3pillarglobal.com'
-                            subject = "Access keys deactivation detail of " + Name
-                            body = "Dear "+ Name + ",\n\n You have not used your access keys in last 3 months. As per the policy your keys has been deactived.\n Please go to your AWS console for more details.\n\nRegards,\nAnkit Tyagi" 
+                    res = client.list_access_keys(UserName=key.user_name)
+                    accesskeydate = res['AccessKeyMetadata'][0]['CreateDate'].date()
+                    currentdate = date.today()
+                    active_days = currentdate - accesskeydate
+                    print (active_days.days)
+                    age = str(active_days.days)
+                    if active_days.days > 85 and active_days.days < 91:
+                        print(key.user_name,"user age is ", active_days.days , " days")
+                        Name = key.user_name
+                        username = os.environ['USERNAME']
+                        password = os.environ['PASSWORD']
+                        host = os.environ['SMTPHOST']
+                        port = os.environ['SMTPPORT']
+                        mail_from = os.environ['MAIL_FROM']
+                        #mail_to = os.environ['MAIL_TO']
+                        MAIL_id = 'antyagi291@gmail.com'
+                        recpt = MAIL_id +',itsmesushil.kumar@gmail.com'
+                        mail_to = recpt
+                        print(mail_to)
+                        #reply_to = os.environ['MAIL_TO']
+                        reply_to = 'itsmesushil.kumar@gmail.com'
+                        subject = "Access keys deactivation detail of " + Name
+                        body = "Dear "+ Name + ",\n\n Your IAM access key age is "+ age +" days.\n Please go to your AWS console for more details.\n\nRegards,\nAnkit Tyagi" 
                             
-                            send_mail(host, port, username, password, subject, body, mail_to, mail_from, reply_to)
+                        send_mail(host, port, username, password, subject, body, mail_to, mail_from, reply_to)
                             
